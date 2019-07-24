86c3b1459aa8c78338abf1b8f4d1068d7cc9cdac
CrazyMax <crazy-max@users.noreply.github.com>
2019-07-24 11:48:12 +0200

    Fix deploy on PR

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
-# enable error reporting to the console
 set -e
+
+# skip on PR
+if [[ ! -v TRAVIS_PULL_REQUEST ]]; then
+  echo "INFO: This is a PR, skipping deploy..."
+  exit 0
+fi
 
 # publish to master branch
 cd ./web
