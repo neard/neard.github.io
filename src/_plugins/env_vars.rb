4c3de1c16bc5ba8c28fea0f8101b0da6f8743108
CrazyMax <crazy-max@users.noreply.github.com>
2019-10-16 16:27:48 +0200

    Switch to GitHub Actions

---
+++ src/_plugins/env_vars.rb
@@ -0,0 +1,10 @@
+module Jekyll
+
+  class EnvVariables < Generator
+    def generate(site)
+      site.config['env'] = {}
+      ENV.each_pair { |k, v| site.config['env'][k] = v }
+    end
+  end
+
+end
